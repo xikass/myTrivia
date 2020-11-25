e94c473fb86338e9c8a79f04420f3849d65f9dfa
Mohammad A. Zachariah <mohammad.a.zachariah@gmail.com>
2020-11-25 16:21:30 +0200

    header.js edit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,7 +12,7 @@ class Header extends Component {
     return (
       <div className="App-header">
         <h1 onClick={() => {this.navTo('')}}>Udacitrivia</h1>
-        <h2 onClick={() => {this.navTo('')}}>List</h2>
+        <h2 onClick={() => {this.navTo('/questions')}}>List</h2>
         <h2 onClick={() => {this.navTo('/add')}}>Add</h2>
         <h2 onClick={() => {this.navTo('/play')}}>Play</h2>
       </div>
